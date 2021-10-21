e61ce995562249337c282321854d8ad9413490cc
Daniel Ferreira <daniel.dlimaf@gmail.com>
2021-10-20 22:24:31 -0300

    Splitted screen

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let screenWidthList = [];
+
 server.listen(8080, () => {
   console.log('listening on *:8080');
 });
@@ -12,9 +14,28 @@ server.listen(8080, () => {
 app.use(express.static(__dirname + "/public"))
 
 io.on('connection', (socket) => {
-    console.log('New conection')
+    const connectionScreenWidth = parseInt(socket.handshake.query.screenWidth);
+
+    // TODO: improve 
+    if (connectionScreenWidth && !screenWidthList.map(item => item.id).includes(socket.id)) {
+      const newItem = {
+        id: socket.id,
+        screenWidth: connectionScreenWidth
+      }
+
+      screenWidthList = [...screenWidthList, newItem];
+    }
+    
+    
+
+    console.log('New conection ->', screenWidthList);
 
     socket.on('draw', (item)=>{
-        io.emit('updateDraw', item)
+        if (item.end.x > screenWidthList[2].screenWidth) {
+          io.emit('updateDraw', {...item, direction: screenWidthList[3].id, firstSize: screenWidthList[2].screenWidth})
+        } else {
+          io.emit('updateDraw', {...item, direction: screenWidthList[2].id})
+        }
+        
     })
 })
