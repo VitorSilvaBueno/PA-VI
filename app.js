35cf621a23544736463ac170210f104b32fde8cb
Daniel Ferreira <daniel.dlimaf@gmail.com>
2021-10-22 18:56:58 -0300

    Updated to support two tabs

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(express.static(__dirname + "/public"))
 io.on('connection', (socket) => {
     const connectionScreenWidth = parseInt(socket.handshake.query.screenWidth);
 
-    // TODO: improve 
     if (connectionScreenWidth && !screenWidthList.map(item => item.id).includes(socket.id)) {
       const newItem = {
         id: socket.id,
@@ -24,18 +23,19 @@ io.on('connection', (socket) => {
       }
 
       screenWidthList = [...screenWidthList, newItem];
+
+      if (screenWidthList.length > 2){
+        screenWidthList = screenWidthList.filter((_, index) => index !== 0);
+      }
     }
-    
-    
 
     console.log('New conection ->', screenWidthList);
 
     socket.on('draw', (item) => {
-        if (item.end.x > screenWidthList[2].screenWidth) {
-          io.emit('updateDraw', {...item, direction: screenWidthList[3].id, firstSize: screenWidthList[2].screenWidth})
-        } else {
-          io.emit('updateDraw', {...item, direction: screenWidthList[2].id})
+        if (screenWidthList[0] && item.end.x > screenWidthList[0].screenWidth) {
+          io.emit('updateDraw', {...item, direction: screenWidthList[1].id, firstSize: screenWidthList[0].screenWidth})
+        } else if (screenWidthList[0]){
+          io.emit('updateDraw', {...item, direction: screenWidthList[0].id})
         }
-        
     })
 })
